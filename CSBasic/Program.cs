b8424740503dcc1385ef81ae87b3c661bd349381
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-04-28 14:21:51 +0900

    C# 기본 연습

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,26 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            //273;
+            var name = "최" + "다" + "연";
+            Console.Write("Hello C#");
+
+            //int if = 1;
+
+            Console.WriteLine("Hello World");
+            Console.Write("Hello World");
+            Console.WriteLine(53);
+            Console.WriteLine('A');
+            Console.WriteLine("A입니다");
+            Console.WriteLine("\"A\"입니다");
+            Console.WriteLine("최" + "다" + "연");
+            //Console.WriteLine("안녕"[100]);
+
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
+
+
         }
     }
 }
